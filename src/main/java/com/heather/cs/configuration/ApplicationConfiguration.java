5c06dbab54d0e76fa131cfd4ea93e31706c8fcba
HanInJu <haninju95@gmail.com>
2022-03-01 02:19:35 +0900

    [ITERATION-3, 한인주] 아규먼트 리졸버 - @LongInUser annotation 생성 - Interceptor에서 User 객체 setAttribute 할 수 있도록 변경 - selectActiveUser 로직 제거 : 수정중

--- src/main/java/com/heather/cs/configuration/ApplicationConfiguration.java
+++ src/main/java/com/heather/cs/configuration/ApplicationConfiguration.java
@@ -1,18 +1,22 @@
 package com.heather.cs.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+import com.heather.cs.argumentresolver.UserArgumentResolver;
 import com.heather.cs.interceptor.AuthInterceptor;
+import com.heather.cs.user.mapper.UserMapper;
 
 @Configuration
 @Import({DatabaseConfiguration.class})
@@ -21,16 +25,24 @@ import com.heather.cs.interceptor.AuthInterceptor;
 @ComponentScan({"com.heather.cs.*"})
 public class ApplicationConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-            .excludePathPatterns("/user")
-            .excludePathPatterns("/logIn");
-    }
+	@Autowired
+	private UserMapper userMapper;
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
-    }
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new UserArgumentResolver());
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuthInterceptor(userMapper))
+			.excludePathPatterns("/user")
+			.excludePathPatterns("/logIn");
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+	}
 }
 
