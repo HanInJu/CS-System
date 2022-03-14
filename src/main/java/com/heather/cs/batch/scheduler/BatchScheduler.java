1d97ae0cd024d63c8250abac5ff2f8ec70ce55e7
HanInJu <haninju95@gmail.com>
2022-03-15 02:22:59 +0900

    [ITERATION-4, 한인주] 배치 구현 - 스케줄링 구현 - 상담원 상태변경(퇴근) : 매일 19시마다 - 지연된 문의 카테고리 변경 : 매 1시간마다

--- src/main/java/com/heather/cs/batch/scheduler/BatchScheduler.java
+++ src/main/java/com/heather/cs/batch/scheduler/BatchScheduler.java
@@ -1,4 +1,46 @@
 package com.heather.cs.batch.scheduler;
 
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.heather.cs.counsel.batch.configuration.DelayedCounselBatchConfiguration;
+import com.heather.cs.user.batch.configuration.CounselorBatchConfiguration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
 public class BatchScheduler {
+
+	private final JobLauncher jobLauncher;
+	private final CounselorBatchConfiguration counselorBatchConfiguration;
+	private final DelayedCounselBatchConfiguration delayedCounselBatchConfiguration;
+
+	@Scheduled(cron = "0 0 19 * * *")
+	public void runChangeUserStatusJob() {
+		try {
+			jobLauncher.run(counselorBatchConfiguration.counselorStatusOffJob(), new JobParameters());
+		} catch (Exception exception) {
+			log.error(exception.getMessage());
+		}
+	}
+
+	@Scheduled(cron = "0 0 */1 * * *")
+	public void runChangeCategoryJob() {
+		try {
+			jobLauncher.run(delayedCounselBatchConfiguration.changeCategoryJob(), new JobParameters());
+		} catch (Exception exception) {
+			log.error(exception.getMessage());
+		}
+	}
 }
