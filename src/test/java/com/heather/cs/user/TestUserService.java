6f7555dc051c15d5830f8150fa6fabbb90818f2d
HanInJu <haninju95@gmail.com>
2022-03-24 21:36:09 +0900

    [ITERATION-5, 한인주] TC 작성 - TestUserService -- registerUser() 테스트

---
+++ src/test/java/com/heather/cs/user/TestUserService.java
@@ -0,0 +1,38 @@
+package com.heather.cs.user;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.heather.cs.user.dto.User;
+import com.heather.cs.user.mapper.UserMapper;
+import com.heather.cs.user.service.UserService;
+
+@ExtendWith(MockitoExtension.class)
+public class TestUserService {
+
+	@InjectMocks
+	private UserService userService;
+
+	@Mock
+	private UserMapper userMapper;
+
+	@Test
+	@DisplayName("Test to register User")
+	public void testRegisterUser() {
+		User user = new User();
+		user.setId("heather");
+		user.setPassword("qwerty");
+
+		Mockito.when(userMapper.selectExistsUserId(user.getId())).thenReturn(false);
+		Mockito.doNothing().when(userMapper).insertUser(user);
+		Mockito.doNothing().when(userMapper).insertUserHistory(user.getId());
+
+		Assertions.assertDoesNotThrow(() -> userService.registerUser(user));
+	}
+}
